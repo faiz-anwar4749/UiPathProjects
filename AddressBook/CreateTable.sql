5bbad3bb28faa7bd86d44149476407b84ed1d62c
Faiz <faiz.anwar4749@gmail.com>
2020-11-28 12:34:20 +0530

    [Faiz] Add: UiPath Practice Problem- Actions on DB

---
+++ AddressBook/CreateTable.sql
@@ -0,0 +1,14 @@
+create table AddressBook(
+FirstName varchar(200),
+LastName varchar(200),
+FatherName varchar(200),
+MotherName varchar(200),
+BloodGroup varchar(200),
+Address varchar(200),
+City varchar(200),
+State varchar(200),
+Zip varchar(200),
+Phone varchar(200),
+Email varchar(200)
+);
+select * from AddressBook;
